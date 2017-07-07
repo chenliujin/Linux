d330997bf023de82eebc8ee8c15238d7d8565df9
chenliujin <liujin.chen@qq.com>
2017-07-07 11:15:23 +0800

    init

--- centos-7/init.sh
+++ centos-7/init.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-# $ vi /etc/sysconfig/network-scripts/ifcfg-eth0
-#   ONBOOT=yes
-# $ ifup eth0
+# network
+sed -i "s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-eth0
+ifup eth0
 
-yum install -y rsync
+yum install -y rsync tree
 yum install -y net-tools
-yum install -y vim
+yum install -y vim screen
+yum install -y epel-release
+#yum install -y git
+
+# 时间同步
+yum install -y ntp
+systemctl enable ntpd
+systemctl start ntpd
+
+## selinux
+setenforce 0
+sed -i "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
+
+# iptables
+
